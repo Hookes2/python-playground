77912fc2d4f6858b01ca3e8b153b956f222cc5b0
Abayomi Obawomiye <abayomi.obawomiye@ey.com>
2022-08-22 11:28:25 -0400

    added data_types and comments

---
+++ 2_data_types.py
@@ -0,0 +1,15 @@
+#   int
+#   float
+#   str
+#   bool ->    True, False
+#   None
+
+print("hello")
+print(4)
+print('hello')
+
+print(type("True"))
+print(type(2))
+print(type("hello"))
+print(type(None))
+print(type(1.23))
---
+++ 3_comments.py
@@ -0,0 +1,15 @@
+print("Hello World!")   #this is a single line comment
+
+'''
+Hello this
+is a multiline
+comment
+
+'''
+
+"""
+Hello this
+is a multiline
+comment
+
+"""
